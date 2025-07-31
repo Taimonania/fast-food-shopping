138cda18517e0a2b26fb92d2bf9dc207caf5e7d2
Timo Angerer <taimonania+github@gmail.com>
2025-07-31 20:55:06 +0200

    new setup with nextjs, shadcn

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fast Food Shopping",
+  description: "Fast Food Shopping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
