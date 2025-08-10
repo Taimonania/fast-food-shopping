bebd2e2d93fd64818a3ad90c85096bd3329ced07
Timo Angerer <taimonania+github@gmail.com>
2025-08-10 10:29:44 +0200

    Check in convex _generated

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,42 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as functions_item from "../functions/item.js";
+import type * as functions_supermarket from "../functions/supermarket.js";
+import type * as tables_item from "../tables/item.js";
+import type * as tables_supermarket from "../tables/supermarket.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  "functions/item": typeof functions_item;
+  "functions/supermarket": typeof functions_supermarket;
+  "tables/item": typeof tables_item;
+  "tables/supermarket": typeof tables_supermarket;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
